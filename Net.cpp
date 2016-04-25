f418cc88c25cca6a9c4a32964a8d5b02331b320e
Benny O <opw0011@gmail.com>
2016-04-25 18:45:23 +0800

    Net.cpp add basic structure

--- Net.cpp
+++ Net.cpp
@@ -1,2 +1,21 @@
 #include "Net.h"
 
+Net::Net(const std::vector<unsigned> &topology, const double eta){
+   std::cout << "net constructor" << std::endl; 
+}
+
+void Net::feedForward(const std::vector<double> &inputVals) {
+   std::cout << "feed forward" << std::endl;
+}
+
+void Net::backProp(const std::vector<double> &targetVals) {
+   std::cout << "back prop" << std::endl;
+}
+
+void Net::getResults(std::vector<double> &resultVals) const {
+    std::cout << "result" << std::endl;
+}
+
+double Net::getError(void) const {
+    std::cout << "error" << std::endl;
+}
